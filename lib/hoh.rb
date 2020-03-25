9f2a1fc9cecfa471118d6409587bf37b404bb29d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-25 22:17:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
 #   sub_category: nil
 # }
 #
-#def naming_system
+def naming_system
   # Remember:
   #  Kingdom
   #  Phylum
@@ -44,32 +44,29 @@
   # HoH all the way to Species!
 
 
-world = {
+earth = {
    label: "Kingdom",
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
-     }
-    
-   }
- }
-
-PORTION_2 = {
-   label: "Order"
- }
- 
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
- 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
+       label: "Class",
+       sub_category: {
+        label: "Order",
+         sub_category: {
+           label: "Family",
+            sub_category: {
+             label: "Genus",
+              sub_category: {
+                label: "Species",
+                  sub_category: nil
+            }
+          }
+        }
+      }
+    }
+  }
 }
-end
 
+earth
+end
 naming_system
